411c9fdf037590768faf1ef49ba4a2e26903b3d7
alexander65536 <anshival@bu.edu>
2020-04-02 00:36:06 -0400

    working mergeSort

--- views/components/sorting_visualizer.js
+++ views/components/sorting_visualizer.js
@@ -29,9 +29,39 @@ class SortingVisualizer extends Component {
 
     mergeSortWrapper() {
         const currentState = [...this.state.array];
-        const sortedState = mergeSort(currentState);
-        console.log(sortedState);
-        this.setState({ array: sortedState });
+        const animations = mergeSort(currentState);
+        const barsFromDom = document.getElementsByClassName("number-bar");
+        for (let i = 0; i < animations.length; i++) {
+            let [firstBar, secondBar] = animations[i][0];
+            setTimeout(() => {
+                barsFromDom[firstBar].style["background-color"] = `aqua`;
+                barsFromDom[secondBar].style["background-color"] = `aqua`;
+            }, i * 24);
+            if (animations[i].length == 2) {
+                setTimeout(() => {
+                    barsFromDom[firstBar].style["background-color"] = `red`;
+                    barsFromDom[secondBar].style["background-color"] = `red`;
+                }, i * 24 + 6);
+
+                const [newIndex, newHeight] = animations[i][1];
+
+                setTimeout(() => {
+                    barsFromDom[newIndex].style.height = `${newHeight}px`;
+                    barsFromDom[newIndex].style["background-color"] = `red`;
+                }, i * 24 + 12);
+
+                setTimeout(() => {
+                    barsFromDom[firstBar].style["background-color"] = `blue`;
+                    barsFromDom[secondBar].style["background-color"] = `blue`;
+                    barsFromDom[newIndex].style["background-color"] = `blue`;
+                }, i * 24 + 18);
+            } else {
+                setTimeout(() => {
+                    barsFromDom[firstBar].style["background-color"] = `blue`;
+                    barsFromDom[secondBar].style["background-color"] = `blue`;
+                }, i * 24 + 12);
+            }
+        }
     }
 
     render() {
--- views/sortingAlgorithms/mergeSort.js
+++ views/sortingAlgorithms/mergeSort.js
@@ -1,36 +1,78 @@
 const mergeSort = nums => {
-    if (nums.length === 1) return nums;
+    const animations = [];
+    let auxiliaryArray = [...nums];
+    mergeSortHelper(nums, auxiliaryArray, 0, nums.length - 1, animations);
+    return animations;
+};
 
-    let mid = Math.floor(nums.length / 2);
+const mergeSortHelper = (
+    nums,
+    auxiliaryArray,
+    startIndex,
+    endIndex,
+    animations
+) => {
+    if (startIndex === endIndex) return;
+    let middleIndex = Math.floor((startIndex + endIndex) / 2) + 1;
+    mergeSortHelper(
+        auxiliaryArray,
+        nums,
+        startIndex,
+        middleIndex - 1,
+        animations
+    );
+    mergeSortHelper(auxiliaryArray, nums, middleIndex, endIndex, animations);
+    doMerge(
+        nums,
+        auxiliaryArray,
+        startIndex,
+        middleIndex,
+        endIndex,
+        animations
+    );
+};
 
-    const leftArray = mergeSort(nums.slice(0, mid));
-    const rightArray = mergeSort(nums.slice(mid));
-    const resultantArray = [];
+const doMerge = (
+    nums,
+    auxiliaryArray,
+    startIndex,
+    middleIndex,
+    endIndex,
+    animations
+) => {
+    let i = startIndex,
+        j = middleIndex,
+        k = startIndex;
 
-    let i = 0,
-        j = 0;
-
-    while (i < leftArray.length && j < rightArray.length) {
-        if (leftArray[i] <= rightArray[j]) {
-            resultantArray.push(leftArray[i]);
-            i++;
+    while (i < middleIndex && j <= endIndex) {
+        animations.push([[i, j]]);
+        if (auxiliaryArray[i] > auxiliaryArray[j]) {
+            if (k != j) {
+                animations[animations.length - 1].push([k, auxiliaryArray[j]]);
+            }
+            nums[k++] = auxiliaryArray[j++];
         } else {
-            resultantArray.push(rightArray[j]);
-            j++;
+            if (k != i) {
+                animations[animations.length - 1].push([k, auxiliaryArray[i]]);
+            }
+            nums[k++] = auxiliaryArray[i++];
         }
     }
 
-    while (i < leftArray.length) {
-        resultantArray.push(leftArray[i]);
-        i++;
+    while (i < middleIndex) {
+        animations.push([[i, i]]);
+        if (k != i) {
+            animations[animations.length - 1].push([k, auxiliaryArray[i]]);
+        }
+        nums[k++] = auxiliaryArray[i++];
     }
-
-    while (j < rightArray.length) {
-        resultantArray.push(rightArray[j]);
-        j++;
+    while (j <= endIndex) {
+        animations.push([[j, j]]);
+        if (k != j) {
+            animations[animations.length - 1].push([k, auxiliaryArray[j]]);
+        }
+        nums[k++] = auxiliaryArray[j++];
     }
-
-    return resultantArray;
 };
 
 export default mergeSort;
